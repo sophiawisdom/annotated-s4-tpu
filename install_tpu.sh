6994ec5184da10eac10b7233c45f87051d3d982e
sophiawisdom <sophiawisdom1999@gmail.com>
2022-05-22 21:06:40 -0700

    we

--- install_tpu.sh
+++ install_tpu.sh
@@ -1,3 +1,3 @@
 pip3 install https://storage.googleapis.com/cloud-tpu-tpuvm-artifacts/wheels/libtpu-nightly/libtpu_nightly-0.1.dev20220522-py3-none-any.whl
-pip3 install jax[tpu]==0.2.26
+pip3 install jax[tpu]
 pip3 install -r requirements-tpu.txt
