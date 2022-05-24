e924093ed58571c0c5fae3f20e12492d73a81f51
sophiawisdom <sophiawisdom1999@gmail.com>
2022-05-23 21:40:38 -0700

    py-spy

--- install_tpu.sh
+++ install_tpu.sh
@@ -1,4 +1,7 @@
 pip3 install https://storage.googleapis.com/cloud-tpu-tpuvm-artifacts/wheels/libtpu-nightly/libtpu_nightly-0.1.dev20220522-py3-none-any.whl
 pip3 install jax[tpu]
+pip3 install py-spy
 pip3 install tensorboard-plugin-profile
 pip3 install -r requirements-tpu.txt
+
+# git clone https://github.com/sophiawisdom/annotated-s4-tpu; cd annotated-s4-tpu; bash install_tpu.sh
